219b4d4017729709c95df79e1b93aadbd7828f17
Raven2004-prog <raven161204@gmail.com>
2024-12-07 08:28:32 +0530

    asdf
    asad

---
+++ Horror/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+   
+    void Start()
+    {
+        // Unlock the cursor and make it visible
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
+
